e9e5dcbcc186fd7b9f013c84784918c91490dad9
mahmudunnabiRain <mahmudunnabirain@gmail.com>
2021-02-18 11:39:58 +0600

    update

--- JokesWebApp/Models/Joke.cs
+++ JokesWebApp/Models/Joke.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace JokesWebApp.Models
 {
     public class Joke
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Joke Question")]
         public string JokeQuestion { get; set; }
+
+        [Required]
+        [Display(Name = "Joke Answer")]
         public string JokeAnswer { get; set; }
 
         public Joke()
